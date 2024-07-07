f30279dad71a830af7d8bc4d06cb309162df1151
Axel Ramge <axel@ramge.de>
2024-07-07 16:55:13 +0200

    permanant storage

--- log_gpsbabel.sh
+++ log_gpsbabel.sh
@@ -13,7 +13,7 @@ then
     -x track,merge \
     -x simplify,crosstrack,error=0.01k \
     -o gpx \
-    -F /media/ramdisk/gpsd_"$(TZ=z date +%Y-%m-%dT%H:%M:%SZ)".gpx
+    -F /home/ramge/local/tracks/gpsd_"$(TZ=z date +%Y-%m-%dT%H:%M:%SZ)".gpx
   echo "$FILES" | xargs rm 
 fi
 
--- log_gpspipe.sh
+++ log_gpspipe.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-gpspipe -r -x 60 \
+gpspipe -r -x 3600 \
   | grep '$GNGGA\|$GNRMC' \
   | tail -n +4 \
   > "/media/ramdisk/gpsd_$(TZ=z date +%Y-%m-%dT%H:%M:%SZ).nmea"
