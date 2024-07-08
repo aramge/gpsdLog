0013644f4fae4555741caa528bddb9165630e60e
Axel Ramge <axel@ramge.de>
2024-07-08 08:39:55 +0200

    rm fixTime.sh

--- fixTime.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-LATEST=$(sed -rn 's/.*<time>([0-9]+.*Z).*/\1/p' "$1" | tail -n1)
-cat "$1" \
-  | sed "s/<time>.*<\/time>/<time>$LATEST<\/time>/" \
-  > gpsd_$LATEST.gpx
-echo $LATEST
